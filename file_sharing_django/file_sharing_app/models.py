7eca8ca24c246c3866c35e587c53a29278e3944f
Baptiste NOGARET <baptiste.nogaret@gmail.com>
2016-03-11 17:52:06 +0000

    Create Group model

--- file_sharing_django/file_sharing_app/models.py
+++ file_sharing_django/file_sharing_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Group(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    members = models.ManyToManyField(User, related_name="members")
+    name = models.CharField(max_length=200)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_last_modification = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
