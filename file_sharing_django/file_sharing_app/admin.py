16c918f294cf045695561e96bf5e7f7ccb99ebd4
Baptiste NOGARET <baptiste.nogaret@gmail.com>
2016-03-25 14:23:29 +0000

    Define my own many to many table for group

--- file_sharing_django/file_sharing_app/admin.py
+++ file_sharing_django/file_sharing_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from . models import Group
+from . models import Group, GroupMembership
 
 # Register your models here.
 
 admin.site.register(Group)
+admin.site.register(GroupMembership)
--- file_sharing_django/file_sharing_app/models.py
+++ file_sharing_django/file_sharing_app/models.py
@@ -5,11 +5,16 @@ from django.contrib.auth.models import User
 
 class Group(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
-    members = models.ManyToManyField(User, related_name="members")
+    members = models.ManyToManyField(User, related_name="members", through='GroupMembership', blank=True)
     name = models.CharField(max_length=200)
     date_creation = models.DateTimeField(auto_now_add=True)
     date_last_modification = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+class GroupMembership(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="group")
+    member = models.ForeignKey(User, on_delete=models.CASCADE, related_name="member")
+    date_joined = models.DateTimeField(auto_now_add=True)
 
